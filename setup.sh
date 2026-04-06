3468aea5ef48c028eca398e4b7c86a7418895cf1
anton <antonmolina@bnext.bio>
2026-04-05 22:39:09 -0700

    handle active conda envs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-
 #!/bin/bash
 # Set up the development environment for building Nucleus docs locally
 
-conda env create -f environment.yml --force
+if [[ "$CONDA_DEFAULT_ENV" == "nucleus-docs" ]]; then
+  echo "Please run 'conda deactivate' first, then re-run this script."
+  exit 1
+fi
+
+conda env remove -n nucleus-docs 2>/dev/null
+conda env create -f environment.yml
 echo ""
 echo "Done! To get started:"
 echo "  conda activate nucleus-docs"
